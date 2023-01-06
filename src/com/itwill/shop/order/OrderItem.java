7a695c4689625588aafc5d1f7c3019fc4a764053
minseonallykim <minsun947@gmail.com>
2023-01-06 19:18:20 +0900

    ㅇㅇ

--- src/com/itwill/shop/order/OrderItem.java
+++ src/com/itwill/shop/order/OrderItem.java
@@ -1,5 +1,20 @@
 package com.itwill.shop.order;
 
-public class OrderItem {
+import com.itwill.shop.product.Product;
 
+/*
+ * 이름     널?       유형         
+	------ -------- ---------- 
+	OI_NO  NOT NULL NUMBER(10) 
+	OI_QTY          NUMBER(10) 
+	P_NO            NUMBER(10) 
+	O_NO            NUMBER(10) 
+ */
+public class OrderItem {
+	private int oi_no;
+	private int oi_qty;
+	/********FK********/
+	private int o_no;
+	/*******FK Product 포함*******/
+	private Product product;
 }
