9279c9edf7a048456014103efe34e86491baf25c
minseonallykim <minsun947@gmail.com>
2023-01-06 19:11:38 +0900

    ..

--- src/com/itwill/shop/cart/Cart.java
+++ src/com/itwill/shop/cart/Cart.java
@@ -1,5 +1,19 @@
 package com.itwill.shop.cart;
 
+import com.itwill.shop.product.Product;
+/*
+ * 이름       널?       유형           
+	-------- -------- ------------ 
+	CART_NO  NOT NULL NUMBER(10)   
+	CART_QTY          NUMBER(10)   
+	M_ID              VARCHAR2(50) 
+	P_NO              NUMBER(10)  
+ */
 public class Cart {
-
+	private int cart_no;
+	private int cart_qty;
+	/************FK*************/
+	private String userid; 
+	/************FK Product 포함*************/
+	private Product product;
 }
--- src/com/itwill/shop/order/Order.java
+++ src/com/itwill/shop/order/Order.java
@@ -1,5 +1,29 @@
 package com.itwill.shop.order;
 
-public class Order {
+import java.util.Date;
+import java.util.List;
 
+/*
+ * 이름        널?       유형             
+	--------- -------- -------------- 
+	O_NO      NOT NULL NUMBER(10)     
+	O_DESC             VARCHAR2(1000) 
+	O_DATE             DATE           
+	O_PRICE            NUMBER(10)     
+	O_ADDRESS          VARCHAR2(500)  
+	O_LOC              VARCHAR2(500)  
+	O_PAYMENT          VARCHAR2(50)   
+	M_ID               VARCHAR2(50)  
+ */
+public class Order {
+	private int o_no;
+	private String o_desc;
+	private Date o_date;
+	private int o_price;
+	private String o_address;
+	private String o_loc;
+	/*********FK**********/
+	private String userid;
+	/******List<OrderItem> 포함 (order 1개에 order_item 여러개 있으니 컬렉션으로)******/
+	private List<OrderItem> orderItemList;
 }
