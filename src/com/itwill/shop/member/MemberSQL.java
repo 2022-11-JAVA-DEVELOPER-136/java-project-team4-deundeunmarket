f593310e7ea489770e4ff08a14e7666aad2c2be1
minseonallykim <minsun947@gmail.com>
2023-01-06 17:53:58 +0900

    ..

--- src/com/itwill/shop/member/MemberSQL.java
+++ src/com/itwill/shop/member/MemberSQL.java
@@ -2,16 +2,7 @@ package com.itwill.shop.member;
 
 
 public class MemberSQL {
-	public static final String MEMBER_INSERT=
-			"insert into member(m_id,m_pass,m_name,m_phone,m_bday,m_email,m_address) values(?,?,?,sysdate,?,?)";
-	public static final String MEMBER_UPDATE=
-			"update member set m_pass=?,m_name=?,m_phone=?,m_bday=?,m_email=?,m_address=?, where m_id=?";
-	public static final String MEMBER_DELETE=
-			"delete from member where m_id=?";
-	public static final String MEMBER_SELECT_BY_ID=
-			"select m_id,m_pass,m_name,m_phone,m_bday,m_email,m_address from member where m_id=?";
-	public static final String MEMBER_SELECT_ALL=
-			"select m_id,m_pass,m_name,m_phone,m_bday,m_email,m_address from member";
+
 }
 
 
--- src/com/itwill/shop/product/ProductSQL.java
+++ src/com/itwill/shop/product/ProductSQL.java
@@ -2,18 +2,4 @@ package com.itwill.shop.product;
 
 public class ProductSQL {
 
-public static final String PRODUCT_INSERT = "insert into product values (product_p_no_seq.nextval,?,?,?,?)";
-
-public static final String PRODUCT_UPDATE 
-	= "update product set p_name = ?, p_price = ?, p_image = ?, p_desc = ? where p_no = ?";
-
-public static final String PRODUCT_DELETE 
-	= "delete from product where p_no=?";
-
-public static final String PRODUCT_SELECT_BY_NO 
-	= "select * from product where p_no=? ";
-
-public static final String PRODUCT_SELECT_ALL 
-	= "select * from product";
-
 }	
