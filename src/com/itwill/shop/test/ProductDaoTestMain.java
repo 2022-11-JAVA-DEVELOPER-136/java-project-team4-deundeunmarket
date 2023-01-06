c9b35a08d713f02d20f64f95fe6dcbbc6ad7c1ac
alterack <kimjun030519@gmail.com>
2023-01-06 16:00:24 +0900

    ..

--- src/com/itwill/shop/test/ProductDaoTestMain.java
+++ src/com/itwill/shop/test/ProductDaoTestMain.java
@@ -1,9 +1,13 @@
 package com.itwill.shop.test;
 
+import com.itwill.shop.product.ProductDao;
+
 public class ProductDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		ProductDao productDao=new ProductDao();
+		System.out.println("findByPrimaryKey >>"+productDao.findByPrimaryKey(1));
+		System.out.println("findAll >>"+productDao.findAll());
 
 	}
 
