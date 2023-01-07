5be5cc3782c88d6cbfefa83cb1168562cadcf93d
minseonallykim <minsun947@gmail.com>
2023-01-07 19:37:11 +0900

    cart 부분 sql문 수정

--- src/com/itwill/shop/erd/shop_select_update_delete.sql
+++ src/com/itwill/shop/erd/shop_select_update_delete.sql
@@ -27,6 +27,7 @@ select * from product where p_no=1;
 select * from cart where m_id = 'sy0';
 -- 로그인 한 회원의 카트 리스트(카트+제품)
 select * from cart c join product p on c.p_no = p.p_no where m_id = 'sy0'; 
+select * from cart c join product p on c.p_no = p.p_no where cart_no = 1; 
 -- 로그인 한 회원의 카트리스트 삭제(cart_no) 
 delete cart where cart_no = 1;
 -- 로그인 한 회원의 카트리스트 전체 삭제(m_id)
@@ -34,9 +35,10 @@ delete cart where m_id = 'sy0';
 -- 로그인 한 회원의 카트 1개 아이템 수량 변경(cart_no)
 update cart set cart_qty = 3 where cart_no = 3;
 -- 로그인 한 회원의 카트에 존재하는 제품의 수(제품 존재여부 판단)
-select count(*) cnt from cart where m_id = 'sy1' and p_no = 3;
+select count(*) cnt from cart c join member m on c.m_id=m.m_id where m.m_id='sy0' and c.p_no=1;
 -- 로그인 한 회원의 카트에 담기(카트에 이미 존재하는 제품의 수량만 수정)
 update cart set cart_qty = cart_qty+1 where m_id = 'sy1' and p_no = 3;
+
 
 
 /************* 주문 ***************/
