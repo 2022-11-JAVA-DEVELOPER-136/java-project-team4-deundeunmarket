bb3d3d098a65372353d9d77d039a53fc0521ee9d
kangcarry <111220895+kangcarry@users.noreply.github.com>
2023-01-08 18:15:52 +0900

    ..

--- src/com/itwill/shop/test/CartDaoTestMain.java
+++ src/com/itwill/shop/test/CartDaoTestMain.java
@@ -1,10 +1,42 @@
 package com.itwill.shop.test;
 
+import java.util.List;
+
+import com.itwill.shop.cart.Cart;
+import com.itwill.shop.cart.CartDao;
+import com.itwill.shop.product.Product;
+import com.itwill.shop.product.ProductService;
+
 public class CartDaoTestMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	public static void main(String[] args) throws Exception {
+		
+		CartDao cartDao = new CartDao();
+		ProductService productService = new ProductService();
+		//1 카트추가
+		System.out.println("카트추가>> "+cartDao.insert(new Cart(0, 2, "sy1", productService.findByPrimaryKey(1))));
+		
+		//2 id, 상품종류로 카트제품존재(개수)확인
+		System.out.println("카트개수>>"+cartDao.countByProductNo("sy1", 1));
+		
+		//3 상품에서 카트 추가시 업데이트
+		System.out.println("상품에서 카트 업데이트>>"+cartDao.updateByProductNo(new Cart(0, 1, "sy1", productService.findByPrimaryKey(1))));
+		
+		//4 카트리스트에서 수정
+		System.out.println("카트 내부에서 업데이트>>"+cartDao.updateByCartNo(new Cart(22, 1, "sy1", productService.findByPrimaryKey(1))));
+		
+		//5 카트  id통해 전체보기
+		List<Cart> cartList1 = cartDao.findByM_Id("sy1");
+		System.out.println(cartList1);
+		
+		//6 카트 id내 전체삭제
+		System.out.println("카트전체삭제>>"+cartDao.deleteByUserId("sy2"));
+		
+		//7 카트 번호로 1개 삭제
+		System.out.println("카트번호삭제>>"+cartDao.deleteByCartNo(25));
+		
+		
+		
 	}
 
 }
--- src/com/itwill/shop/test/CartServiceTestMain.java
+++ src/com/itwill/shop/test/CartServiceTestMain.java
@@ -1,10 +1,41 @@
 package com.itwill.shop.test;
 
+import com.itwill.shop.cart.Cart;
+import com.itwill.shop.cart.CartService;
+import com.itwill.shop.product.Product;
+import com.itwill.shop.product.ProductService;
+
 public class CartServiceTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		CartService cartService = new CartService();
+		ProductService productService = new ProductService();
+		
+	//  카트 추가 or 수정 - addCart
+		System.out.println("addCart >> "+cartService.addCart(new Cart(0, 0, "sy2",
+									productService.findByPrimaryKey(1))));
+		
+	//카트 아이템 1개 보기 - getCartItemByCartNo	
+		Cart findCart = cartService.getCartItemByCartNo(10);
+	System.out.println("getCartItemByCartNo >> "+findCart);		
+		
+	
+	//카트 수량 변경 -  updateCart
+		findCart.setCart_qty(findCart.getCart_qty()+1);
+		System.out.println("updateCart >> "+cartService.updateCart(findCart));
+	 	
+	//카트 전체보기	- getCartItemByUserId
+		System.out.println("getCartItemByUserId >> "+cartService.getCartItemByUserId("sy1"));
 
+	
+	//카트 아이템 1개 삭제 - deleteCartItemByCartNo
+		System.out.println("deleteCartItemByCartNo >> "+cartService.deleteCartItemByCartNo(29));
+		
+	//카트 전체 삭제- deleteCartItemByUserId	
+		System.out.println("deleteCartItemByUserId >> "+cartService.deleteCartItemByUserId("sy2"));
+		
+		
 	}
 
 }
