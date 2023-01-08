12bd650fc9660adf1d067c3022f0e6eb9489d80c
Motte <Motte@192.168.200.136>
2023-01-08 17:44:17 +0900

    멤버PACKAGE 4개 파일/ 테스트PACKAGE - MEMBERDAOTEST,MEMBERSERVICETEST 업로드

--- src/com/itwill/shop/member/MemberService.java
+++ src/com/itwill/shop/member/MemberService.java
@@ -1,33 +1,113 @@
 package com.itwill.shop.member;
 
+import java.util.List;
+
 public class MemberService {
+	private MemberDao memberDao;
+	public MemberService() throws Exception {
+		memberDao=new MemberDao();
+	}
 /*
  * 회원가입- addMember
  */
+	public boolean create(Member member) throws Exception{
+		boolean isSuccess=false;
+		//1.아이디중복확인
+		if (memberDao.findByPrimaryKey(member.getM_id())==null) {
+			int rowCount=memberDao.insert(member);
+			isSuccess=true;
+		}else {
+			isSuccess=false;
+		}
+		
+		return isSuccess;
+	}
+	
 /*
  * 회원로그인 - logIn	
  */
+	public int login(String m_id, String m_pass) throws Exception {
+		/*
+		 * 0 : 해당하는 아이디 없음 - 로그인 실패 
+		 * 1 : 일치하는 아이디 있음 -로그인 성공
+		 */
+		int result=0;
+		if (memberDao.countByMemberId(m_id)==1) {
+			Member loginMember = memberDao.findByPrimaryKey(m_id);
+			if (loginMember.getM_pass().equals(m_pass)) {
+				result =1; //일치
+			}else {
+				result=0; //없음
+			}
+		}else {
+			result=0;
+		}
+		return result;
+	}
+	
 /*
  * 회원아이디로 검색 - 아이디 중복 확인용 - isDuplicateId
  */
+	public boolean isDuplicateId(String m_id) throws Exception{
+		if(memberDao.countByMemberId(m_id)>=1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 /*
- * 비밀번호 중복 확인
+ * 비밀번호 중복 확인 - 비밀번호 중복 확인용 - isDuplicatePw
  */
+	public boolean isDuplicatePw(String m_pw) throws Exception{
+		if(memberDao.countByMemberPw(m_pw)>=1) {
+			return true; //비밀번호 존재 -> 입력한 비밀번호랑 같다는 말(비밀번호확인)
+		}else {
+			return false; //비밀번호 미존재 ->입력한 비밀번호 다르다는 말(비밀번호불일치)
+		}
+	}
+		
 /*
  * 회원 상세보기 - memberDetail
  */
+	 public Member findMemberDetail(String m_id) throws Exception{
+		 return memberDao.findByPrimaryKey(m_id);
+	 }
+	
+	
+	
+	
 /*
  * 회원추가 - memberWrite
  */
+	public int memberWrite(Member meber) throws Exception {
+		return memberDao.insert(meber);
+	}
+	
+	
+	
 /*
  * 회원정보 수정 - memberUpdate
  */
+	public int update(Member meber) throws Exception {
+		return memberDao.update(meber);
+	}
+	
+	
+	
 /*
  * 회원아이디로 삭제(회원탈퇴) - memberDelete
  */
-/*
- * 회원리스트 보기 - memberList
- */
+	public int remove(String m_id) throws Exception {
+		return memberDao.delete(m_id);
+	}
 	
+	
+	
+/*
+ * 회원리스트 보기 - memberList(관리자)
+ */
+	public List<Member> memberList() throws Exception{
+		return memberDao.findAll();
+	}
 	
 }
--- src/com/itwill/shop/test/MemberDaoTestMain.java
+++ src/com/itwill/shop/test/MemberDaoTestMain.java
@@ -1,10 +1,37 @@
 package com.itwill.shop.test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.print.SimpleDoc;
+
+import com.itwill.shop.member.Member;
+import com.itwill.shop.member.MemberDao;
+
 public class MemberDaoTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		MemberDao memberDao=new MemberDao();
+		
+		//Member memeber = new Member("myt10", "myt9", "최민영", "010-0000-0000", new Date(1994,01,01), "cmy@naver.com", "서울시 동대문구");
+		//int rowCount=memberDao.insert(memeber);
+		//System.out.println("1. insert -->"+rowCount);
+		
+		int rowCount=memberDao.delete("myt10");
+		System.out.println("2. delete -->"+rowCount);
+		
+		
+		Member member = new Member("sy2", "pass", "테스트", "phonenumber", new Date(1999,01,01), "text1", "Text2");
+		rowCount=memberDao.update(member);
+		System.out.println("3. update -->"+rowCount);
+		
+		System.out.println("4. select -->"+memberDao.findByPrimaryKey("sy2"));
+		
+		List<Member> memberList = memberDao.findAll();
+		System.out.println("5. selectAll -->"+memberList);
+		
+		
 	}
 
 }
