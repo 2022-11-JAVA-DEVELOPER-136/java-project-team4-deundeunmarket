990d3db8d29047e1e4c911ff92ed90e7400f315c
KHS <KHS@14.33.90.200>
2023-01-08 18:03:44 +0900

    product

--- src/com/itwill/shop/product/ProductService.java
+++ src/com/itwill/shop/product/ProductService.java
@@ -1,11 +1,28 @@
 package com.itwill.shop.product;
 
+import java.util.List;
+
 public class ProductService {
-/*
- * 전체 상품 보기- productList
- */
+	private ProductDao productDao;
 	
-/*
- * 상품 상세 보기 - productDetail
- */
+	public ProductService() throws Exception {
+		productDao = new ProductDao();
+	}
+	
+	/*
+	 * 상품 상세 보기 - productDetail
+	 */
+	public Product findByPrimaryKey(int p_no) throws Exception {
+		return productDao.findByPrimaryKey(p_no);
+	}
+	
+	/*
+	 * 전체 상품 보기- productList
+	 */
+	
+	public List<Product> findAll() throws Exception {
+		return productDao.findAll();
+	}
+	
+	
 }
--- src/com/itwill/shop/test/ProductServiceTestMain.java
+++ src/com/itwill/shop/test/ProductServiceTestMain.java
@@ -1,10 +1,17 @@
 package com.itwill.shop.test;
 
+import com.itwill.shop.product.ProductService;
+
 public class ProductServiceTestMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		ProductService productService = new ProductService();
+		
+		System.out.println("1.상품리스트 한개");
+		System.out.println(productService.findByPrimaryKey(2));
+		
+		System.out.println("2.상품리스트 전체보기");
+		System.out.println(productService.findAll());
 	}
 
 }
