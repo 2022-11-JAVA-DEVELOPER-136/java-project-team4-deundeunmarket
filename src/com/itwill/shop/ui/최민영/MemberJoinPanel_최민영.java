b6290c2bbca4176c792b746fe0244d67bf9454c4
minyoung.c <devcmy136@gmail.com>
2023-01-09 14:11:46 +0900

    **

--- src/com/itwill/shop/ui/최민영/MemberJoinPanel_최민영.java
+++ src/com/itwill/shop/ui/최민영/MemberJoinPanel_최민영.java
@@ -1,6 +1,9 @@
 package com.itwill.shop.ui.최민영;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
 
 public class MemberJoinPanel_최민영 extends JPanel {
 
@@ -8,7 +11,7 @@ public class MemberJoinPanel_최민영 extends JPanel {
 	 * Create the panel.
 	 */
 	public MemberJoinPanel_최민영() {
+		
 
 	}
-
 }
