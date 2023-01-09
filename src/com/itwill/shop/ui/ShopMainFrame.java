51d73afda6496a096d2233c0d19f5e2d7e6f6c93
minseonallykim <minsun947@gmail.com>
2023-01-09 10:30:12 +0900

    ..

--- src/com/itwill/shop/ui/ShopMainFrame.java
+++ src/com/itwill/shop/ui/ShopMainFrame.java
@@ -6,6 +6,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
+import java.awt.Cursor;
 
 public class ShopMainFrame extends JFrame {
 
@@ -40,7 +45,35 @@ public class ShopMainFrame extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel globalNorthMenuPanel = new JPanel();
+		globalNorthMenuPanel.setBackground(new Color(46, 139, 87));
 		contentPane.add(globalNorthMenuPanel, BorderLayout.NORTH);
+		
+		JPanel globalSouthMenuPanel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) globalSouthMenuPanel.getLayout();
+		flowLayout.setHgap(50);
+		globalSouthMenuPanel.setBackground(new Color(46, 139, 87));
+		contentPane.add(globalSouthMenuPanel, BorderLayout.SOUTH);
+		
+		JButton globalSearchMenuButton = new JButton("");
+		globalSearchMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		globalSearchMenuButton.setBorder(null);
+		globalSearchMenuButton.setOpaque(false);
+		globalSearchMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/든든마켓 화이트50.png")));
+		globalSouthMenuPanel.add(globalSearchMenuButton);
+		
+		JButton globalHomeMenuButton = new JButton("");
+		globalHomeMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		globalHomeMenuButton.setOpaque(false);
+		globalHomeMenuButton.setBorder(null);
+		globalHomeMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/든든마켓 화이트50.png")));
+		globalSouthMenuPanel.add(globalHomeMenuButton);
+		
+		JButton globalMemberMenuButton = new JButton("");
+		globalMemberMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		globalMemberMenuButton.setBorder(null);
+		globalMemberMenuButton.setOpaque(false);
+		globalMemberMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/든든마켓 화이트50.png")));
+		globalSouthMenuPanel.add(globalMemberMenuButton);
 	}
 
 }
