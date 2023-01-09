59559a33268151c302ad319c0858f424a4589736
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-01-09 15:12:34 +0900

    Update MemberJoinPanel_김민영.java

--- src/com/itwill/shop/ui/김민영/MemberJoinPanel_김민영.java
+++ src/com/itwill/shop/ui/김민영/MemberJoinPanel_김민영.java
@@ -1,14 +1,118 @@
 package com.itwill.shop.ui.김민영;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
-public class MemberJoinPanel_김민영 extends JPanel {
+import com.itwill.shop.member.*;
 
+public class MemberLoginPanel_김민영 extends JPanel {
+	private MemberService memberService;
+	private JTabbedPane memberTabbedPane;
+	private JTextField loginIdTF;
+	private JPasswordField loginPassTF;
+	private JMenuItem loginMenuItem;
+	private JMenuItem joinMenuItem;
+	private JMenuItem logoutMenuItem;
+	private JLabel idMsgLB;
+	
+	
+	
+	private Member loginMember=null;
 	/**
 	 * Create the panel.
 	 */
-	public MemberJoinPanel_김민영() {
-
+	public MemberLoginPanel_김민영() {
+		JPanel memberLoginPanel = new JPanel();
+		memberLoginPanel.setBackground(new Color(46, 139, 87));
+		memberTabbedPane.addTab("회원로그인", null, memberLoginPanel, null);
+		memberLoginPanel.setLayout(null);
+		
+		JLabel passwordLB = new JLabel("아이디");
+		passwordLB.setBounds(72, 276, 57, 15);
+		memberLoginPanel.add(passwordLB);
+		
+		JLabel lblNewLabel_6 = new JLabel("패쓰워드");
+		lblNewLabel_6.setBounds(72, 338, 57, 15);
+		memberLoginPanel.add(lblNewLabel_6);
+		
+		loginIdTF = new JTextField();
+		loginIdTF.setBounds(172, 273, 116, 21);
+		memberLoginPanel.add(loginIdTF);
+		loginIdTF.setColumns(10);
+		
+		loginPassTF = new JPasswordField();
+		loginPassTF.setBounds(172, 335, 116, 21);
+		memberLoginPanel.add(loginPassTF);
+		
+		JButton loginBtn = new JButton("로그인");
+		loginBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/***********회원로그인************/
+				try {
+					String id = loginIdTF.getText();
+					String pass=new String(loginPassTF.getPassword());
+					
+					int result = memberService.logIn(id, pass);
+					if(result==0) {
+						//로그인성공
+						loginProcess(id);
+						loginIdTF.setText("");
+						loginPassTF.setText("");
+					}else {
+						JOptionPane.showMessageDialog(null, "아이디또는 비밀번호를 확인하세요");
+						loginIdTF.setSelectionStart(0);
+						loginIdTF.setSelectionEnd(id.length());
+						loginIdTF.requestFocus();
+					}
+					
+					
+					
+				}catch (Exception e1) {
+					// TODO: handle exception
+				}
+			}
+		});
+		loginBtn.setBounds(74, 397, 97, 23);
+		memberLoginPanel.add(loginBtn);
+		
+		JButton joinBtn = new JButton("회원가입");
+		joinBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				memberTabbedPane.setSelectedIndex(1);
+			}
+		});
+		joinBtn.setBounds(191, 397, 97, 23);
+		memberLoginPanel.add(joinBtn);
+	}
+	private void loginProcess(String id) throws Exception{
+		/***********로그인성공시 해야할일***********
+		 1.로그인성공한 멤버객체 멤버필드에저장
+		 2.MemberMainFrame타이틀변경
+		 3.로그인,회원가입탭 불활성화
+		   로그인,회원가입 메뉴아이템 불활성화
+		   로그아웃 메뉴아이템 활성화
+		 4.회원정보보기 화면전환
+		********************************************/
+		//2.MemberMainFrame타이틀변경
+		// 3.로그인,회원가입탭 불활성화
+		memberTabbedPane.setEnabledAt(1, false);
+		memberTabbedPane.setEnabledAt(2, false);
+		memberTabbedPane.setEnabledAt(3, true);
+		loginMenuItem.setEnabled(false);
+		joinMenuItem.setEnabled(false);
+		logoutMenuItem.setEnabled(true);
+		
+		
 	}
 
 }
