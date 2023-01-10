385b89972e437b5c0de30c9d9ce2696b952e065b
minseonallykim <minsun947@gmail.com>
2023-01-10 15:23:39 +0900

    Merge branch 'master' of https://github.com/2022-11-JAVA-DEVELOPER/java-project-team4-deundeunmarket.git

--- src/com/itwill/shop/ui/ShopMainFrame.java
+++ src/com/itwill/shop/ui/ShopMainFrame.java
@@ -24,7 +24,14 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class ShopMainFrame extends JFrame {
-
+	/*
+	 * Panel상수
+	 */
+	public static final int PRODUCT_LIST_PANEL = 1;
+	public static final int PRODUCT_POPULAR_PANEL = 2;
+	public static final int MEMBER_JOIN_PANEL = 3;
+	public static final int MEMBER_INFO_PANEL = 4;
+	
 	private JPanel contentPane;
 	/*
 	 * 1. Service 객체 선언
@@ -37,6 +44,14 @@ public class ShopMainFrame extends JFrame {
 	 * 2. login Member 객체 선언
 	 */
 	Member loginMember = null;
+	
+	private JTabbedPane shopTabbedPane;
+	private PopularProductPanel popularProductPanel;
+	private ProductListPanel productListPanel;
+	private MemberJoinPanel memberJoinPanel;
+	private MemberInfoPanel memberInfoPanel;
+	private JTabbedPane productTabbedPane;
+	private JTabbedPane memberTabbedPane;
 	/**
 	 * Launch the application.
 	 */
@@ -87,15 +102,15 @@ public class ShopMainFrame extends JFrame {
 		contentPane.add(globalSouthMenuPanel, BorderLayout.SOUTH);
 		
 		JButton globalSearchMenuButton = new JButton("");
+		globalSearchMenuButton.setOpaque(false);
 		globalSearchMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		globalSearchMenuButton.setBorder(null);
-		globalSearchMenuButton.setOpaque(false);
 		globalSearchMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/search 50.png")));
 		globalSouthMenuPanel.add(globalSearchMenuButton);
 		
 		JButton globalHomeMenuButton = new JButton("");
-		globalHomeMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		globalHomeMenuButton.setOpaque(false);
+		globalHomeMenuButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		globalHomeMenuButton.setBorder(null);
 		globalHomeMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/home 50.png")));
 		globalSouthMenuPanel.add(globalHomeMenuButton);
@@ -107,20 +122,26 @@ public class ShopMainFrame extends JFrame {
 		globalMemberMenuButton.setIcon(new ImageIcon(ShopMainFrame.class.getResource("/images/cart50.png")));
 		globalSouthMenuPanel.add(globalMemberMenuButton);
 		
-		JTabbedPane shopTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		shopTabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(shopTabbedPane, BorderLayout.CENTER);
 		
-		JTabbedPane productTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		productTabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		shopTabbedPane.addTab("제품", null, productTabbedPane, null);
 		
-		PopularProductPanel popularProductPanel = new PopularProductPanel();
-		productTabbedPane.addTab("인기제품", null, popularProductPanel, null);
+		popularProductPanel = new PopularProductPanel();
+		productTabbedPane.addTab("인기상품", null, popularProductPanel, null);
 		
-		JTabbedPane memberTabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		productListPanel = new ProductListPanel();
+		productTabbedPane.addTab("제품리스트", null, productListPanel, null);
+		
+		memberTabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		shopTabbedPane.addTab("회원", null, memberTabbedPane, null);
 		
-		MemberJoinPanel memberJoinPanel = new MemberJoinPanel();
-		memberTabbedPane.addTab("New tab", null, memberJoinPanel, null);
+		memberJoinPanel = new MemberJoinPanel();
+		memberTabbedPane.addTab("회원가입", null, memberJoinPanel, null);
+		
+		memberInfoPanel = new MemberInfoPanel();
+		memberTabbedPane.addTab("회원정보", null, memberInfoPanel, null);
 		
 		/*****************************/
 		/*
@@ -130,6 +151,15 @@ public class ShopMainFrame extends JFrame {
 		orderService = new OrderService();
 		cartService = new CartService();
 		productService = new ProductService();
+		
+		/****** ShopMainFrame 참조를 Panel에게 넘겨줌 ******/
+		productListPanel.setFrame(this); 
+		popularProductPanel.setFrame(this); 
+		memberInfoPanel.setFrame(this); 
+		memberJoinPanel.setFrame(this);  
+		
+		
+		
 		/*
 		 * 4. login Member 객체 생성
 		 */
@@ -139,4 +169,32 @@ public class ShopMainFrame extends JFrame {
 		
 		
 	}// 생성자 끝
+	/***************패널 변경 메소드******************/
+	
+	public void changePanel(int panel_no) {
+		if (panel_no == PRODUCT_POPULAR_PANEL) {
+			shopTabbedPane.setSelectedIndex(0);
+			productTabbedPane.setSelectedIndex(0);
+		} else if (panel_no == PRODUCT_LIST_PANEL) {
+			shopTabbedPane.setSelectedIndex(0);
+			productTabbedPane.setSelectedIndex(1);
+		} else if (panel_no == MEMBER_JOIN_PANEL) {
+			shopTabbedPane.setSelectedIndex(1);
+			productTabbedPane.setSelectedIndex(1);
+		} else if (panel_no == MEMBER_INFO_PANEL) {
+			shopTabbedPane.setSelectedIndex(1);
+			productTabbedPane.setSelectedIndex(2);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/com/itwill/shop/ui/김민선/OrderCreatePanel_김민선.java
+++ src/com/itwill/shop/ui/김민선/OrderCreatePanel_김민선.java
@@ -9,6 +9,7 @@ import com.itwill.shop.cart.Cart;
 import com.itwill.shop.cart.CartService;
 import com.itwill.shop.member.Member;
 import com.itwill.shop.order.Order;
+import com.itwill.shop.order.OrderItem;
 import com.itwill.shop.order.OrderService;
 import com.itwill.shop.product.Product;
 import com.itwill.shop.product.ProductService;
@@ -18,6 +19,7 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
@@ -37,9 +39,12 @@ public class OrderCreatePanel_김민선 extends JPanel {
 	 * order 객체 선언
 	 */
 	private Order order = null;
+
 	
 	private JTextField orderNameTF;
 	private JTextField orderAddressTF;
+	private JComboBox paymentCB;
+	private JComboBox orderLocCB;
 
 	public OrderCreatePanel_김민선() throws Exception{
 		
@@ -77,13 +82,13 @@ public class OrderCreatePanel_김민선 extends JPanel {
 		OrderPaymentLB.setBounds(45, 322, 57, 15);
 		orderCreatePanel.add(OrderPaymentLB);
 		
-		JComboBox paymentCB = new JComboBox();
+		paymentCB = new JComboBox();
 		paymentCB.setModel(new DefaultComboBoxModel(new String[] {"카드", "계좌이체"}));
 		paymentCB.setEditable(true);
 		paymentCB.setBounds(171, 318, 136, 23);
 		orderCreatePanel.add(paymentCB);
 		
-		JComboBox orderLocCB = new JComboBox();
+		orderLocCB = new JComboBox();
 		orderLocCB.setModel(new DefaultComboBoxModel(new String[] {"문 앞", "경비실", "택배함"}));
 		orderLocCB.setEditable(true);
 		orderLocCB.setBounds(171, 251, 136, 23);
@@ -96,35 +101,16 @@ public class OrderCreatePanel_김민선 extends JPanel {
 		JButton orderCreateBtn = new JButton("주문하기");
 		orderCreateBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
-				try {
-					String o_name = orderNameTF.getText();
-					String o_address = orderAddressTF.getText();
-					String o_payment = (String) paymentCB.getSelectedItem();
-					String o_loc = (String) orderLocCB.getSelectedItem();
-					
-					Order newOrder = new Order(0, o_name, null, null, 0, o_address, o_loc, o_payment, loginMember.getM_id());
-					List<Cart> findCart = cartService.getCartItemByMemberId(loginMember.getM_id());
-					int isCreated;
-					isCreated = orderService.orderCreate(newOrder);
-					if (findCart.size() > 0 && isCreated >= 1) {
-						JOptionPane.showMessageDialog(null, "주문이 완료되었습니다.");
-						// 주문 성공 시 주문 상세보기 화면으로 전환
-						// 주문 상세보기 화면 활성화
-					} 
-					
-				} catch (Exception e1) {
-					e1.getMessage();
-					JOptionPane.showMessageDialog(null, "장바구니가 비어있습니다.");
-				}
-				
-				
+				/*
+				 * 주문 생성 메소드 사용
+				 */
+				orderCreate();
 			}
 		});
 		orderCreateBtn.setBounds(87, 455, 176, 40);
 		orderCreatePanel.add(orderCreateBtn);
 		
-		JLabel orderTotPriceLB = new JLabel("");
+		JLabel orderTotPriceLB = new JLabel("120000");
 		orderTotPriceLB.setHorizontalAlignment(SwingConstants.TRAILING);
 		orderTotPriceLB.setBounds(171, 385, 136, 15);
 		orderCreatePanel.add(orderTotPriceLB);
@@ -142,8 +128,71 @@ public class OrderCreatePanel_김민선 extends JPanel {
 		/*
 		 * order 객체 생성
 		 */
-		order = orderService.orderDetail(loginMember.getM_id(), 1);
+		//order = orderService.orderDetail(loginMember.getM_id(), 3);
+
 		/*****************************************/
 		
 	}
+	/*
+	 * 주문 총 금액 메소드
+	 */
+	public void orderTotPrice(String m_id) throws Exception{
+		List<Cart> cartList = cartService.getCartItemByMemberId(m_id);
+		
+	}
+	/*
+	 public int orderCreate(Order order) throws Exception {
+		
+		List<Cart> cartList = cartDao.findByMemberId(order.getM_id());
+		List<OrderItem> orderItemList = new ArrayList<OrderItem>();
+		int o_tot_price = 0;
+		int oi_tot_count = 0;
+		String o_desc = "";
+		int rowCount = 0;
+		
+		for (Cart cart : cartList) {
+			OrderItem orderItem = new OrderItem(0, cart.getCart_qty(), cart.getProduct(), 0);
+			orderItemList.add(orderItem);
+			o_tot_price += orderItem.getOi_qty()*orderItem.getProduct().getP_price();
+			oi_tot_count += orderItem.getOi_qty();
+		}
+		o_desc = orderItemList.get(0).getProduct().getP_name() + " 외" + (oi_tot_count - 1) + "개";
+		
+		Order newOrder = new Order(0, order.getO_name(),o_desc, null, o_tot_price, order.getO_address(), order.getO_loc(), order.getO_payment(), order.getM_id());
+		newOrder.setOrderItemList(orderItemList);
+		rowCount = orderDao.insert(newOrder);
+		cartDao.deleteByMemberId(order.getM_id());
+		return rowCount;
+	}
+	 */
+	
+	
+	/*
+	 * 주문하기 메소드
+	 */
+	
+	public void orderCreate() {
+		try {
+			String o_name = orderNameTF.getText();
+			String o_address = orderAddressTF.getText();
+			String o_payment = (String) paymentCB.getSelectedItem();
+			String o_loc = (String) orderLocCB.getSelectedItem();
+
+			Order newOrder = new Order(0, o_name, null, null, 0, o_address, o_loc, o_payment, loginMember.getM_id());
+			List<Cart> findCart = cartService.getCartItemByMemberId(loginMember.getM_id());
+			int isCreated;
+			isCreated = orderService.orderCreate(newOrder);
+			if (findCart.size() > 0 && isCreated >= 1) {
+				JOptionPane.showMessageDialog(null, "주문이 완료되었습니다.");
+				// 주문 성공 시 주문 상세보기 화면으로 전환
+				// 주문 상세보기 화면 활성화
+			}
+		} catch (Exception e1) {
+			e1.getMessage();
+			JOptionPane.showMessageDialog(null, "장바구니가 비어있습니다.");
+		}
+	}
+	
+	
+	
 }
